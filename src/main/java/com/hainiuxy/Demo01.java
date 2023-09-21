5daf08b03e00db13b1ab66ae5a8e6c7974213681
hainiu <hainiu@example.com>
2023-09-21 16:29:09 +0800

    这是登录功能的1.0版本

---
+++ src/main/java/com/hainiuxy/Demo01.java
@@ -0,0 +1,13 @@
+package com.hainiuxy;
+
+/**
+ * @author HaiNiu
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+
+        System.out.println(111);
+        System.out.println(222);
+
+    }
+}
